9c9f60bef1a478f5aab83f3b91720dfa4bd5ef7c
josephbenjamin <josephbenjamin7@gmail.com>
2025-01-25 20:12:10 +0000

    second commit

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def main():
-    pass
+    iris = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+    print(iris)
+    fig, ax = plt.subplots()
+    ax.scatter(iris['sepal_length'], iris['sepal_width'])
+    plt.show()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
